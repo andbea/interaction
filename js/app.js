098b846523a0ce0eb69c426008a1301d958be13e
andbea <andbea@kth.se>
2015-02-03 11:36:51 +0100

    Corrected design and added searchbar

--- js/app.js
+++ js/app.js
@@ -6,37 +6,3 @@ $(function() {
 	var exampleView = new ExampleView($("#exampleView"));
 
 });
-
-function load_sidebar() {
-
-}
-
-function hide_sidebar() {
-
-}
-
-function load_searchbar() {
-
-}
-
-function hide_searchbar() {
-
-}
-
-function load_menu_table() {
-
-}
-
-function hide_menu_table() {
-
-}
-
-function load_recipe(item) {
-
-}
-
-function hide_recipe() {
-
-}
-
-function load
