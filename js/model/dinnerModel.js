27e7fcfc1afcdc67fcda4d85e104f73ee5d90e79
mikaelaillanes <mikaela.illanes@gmail.com>
2015-02-03 16:09:51 +0100

    Input number of guests

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -6,7 +6,14 @@ var DinnerModel = function() {
 
 
 	this.setNumberOfGuests = function(num) {
-	
+		var qty = document.getElementById('qty').value;
+		var new_qty = parseInt(qty, 10)+ val;
+
+		if (new_qty < 0) {
+			new_qty = 0;
+		}	
+		document.getElementById('qty').value = new_qty;
+		return new_qty;
 		//TODO Lab 2
 	}
 
