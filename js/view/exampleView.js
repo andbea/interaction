d4b7ba3f18486eec011ed304d93544911d83bc57
andbea <andbea@kth.se>
2015-02-25 10:18:04 +0100

    Dynamicly loaded all relevant data now

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -11,12 +11,21 @@ var ExampleView = function (container, model) {
 	this.confirmDishButton = container.find("#confirmDish");
 	this.confirmDinnerButton = container.find("#confirmDinner");
 	this.fullRecipeButton = container.find("#fullRecipe");
-
+	this.dishTitle = container.find("#dishTitle");
+	this.dishImage = container.find("#dishImage");
+	this.dishDetails = container.find("#dishDetails");
 	this.menuTable = container.find("#menu_table");
+	this.dishIngredients = container.find("#ingredients_content");
+	this.selectedMenu = container.find("#selected_menu");
 	
 	this.numberOfGuests.html(model.getNumberOfGuests());
 	this.dishPrice.html(model.getTotalMenuPrice());
 	this.menuTable.html(model.populateDishTable());
+	this.dishTitle.html(model.getDishTitle(1));
+	this.dishImage.attr("src", model.getDishImage(1));
+	this.dishDetails.html(model.getDishDetails(1));
+	this.dishIngredients.html(model.getIngredientsTable(1));
+	this.selectedMenu.html(model.getMenu([1, 2, 3]));
 
 	model.addObserver(this);
 
