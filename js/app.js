3a8d65803b45569b55b6ee9ff21ce35f7ae0c975
andbea <andbea@kth.se>
2015-02-27 13:52:07 +0100

    More files

--- js/app.js
+++ js/app.js
@@ -3,8 +3,16 @@ $(function() {
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"), model);
-	var exampleViewController = new ExampleViewController(exampleView, model);
+	var firstView = new FirstView($("#firstView"), model);
+	var firstViewController = new FirstViewController(firstView, model);
+
+	var detailsView = new DetailsView($("#detailsView"), model);
+	var detailsViewController = new DetailsViewController(detailsView, model);
  
+	var menuView = new MenuView($("#menuView"), model);
+	var menuViewController = new MenuViewController(menuView, model);
+
+	var overviewView = new overviewView($("#overviewView"), model);
+	var overviewViewController = new overviewViewController(menuView, model);
 });
 
