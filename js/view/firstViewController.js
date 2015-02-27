82f6b0e7dedcff50228158bfe36cbc797d34b8ce
andbea <andbea@kth.se>
2015-02-27 14:10:28 +0100

    Finished firstview

--- js/view/firstViewController.js
+++ js/view/firstViewController.js
@@ -1,23 +1,6 @@
 //ExampleViewController Object constructor
-var ExampleViewController = function(view, model) {
- 	view.plusButton.click(function(){
- 		model.setNumberOfGuests(model.getNumberOfGuests() + 1);
- 	});
- 
- 	view.minusButton.click(function(){
- 		model.setNumberOfGuests(model.getNumberOfGuests() - 1);
- 	});
+var FirstViewController = function(view, model) {
  	view.goToMainButton.click(function(){
- 		window.location.href = "index.html";
- 	});
- 	view.confirmDinnerButton.click(function(){
- 		window.location.href = "dinnerOverview.html";
- 	});
- 	view.goToMainButton.click(function(){
- 		window.location.href = "index.html";
- 	});
-
- 	view.fullRecipeButton.click(function(){
- 		window.location.href = "preparation.html";
+ 		view.css("display", "none");
  	});
 }
