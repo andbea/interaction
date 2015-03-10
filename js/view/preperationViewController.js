b90db45014ac19082983cf1530319f950d8163db
mikaelaillanes <mikaela.illanes@gmail.com>
2015-03-10 11:57:08 +0100

    Small changes in preparation view

--- js/view/preperationViewController.js
+++ js/view/preperationViewController.js
@@ -1,8 +1,11 @@
 //ExampleViewController Object constructor
 var PreperationViewController = function(view, model) {
  	view.goToMainButton.click(function(){
- 		$("#preperationView").css("display", "none");
+ 		$("#preperationView").css("display", "block");
  		$("#sidebarView").css("display", "block");
  		$("#menuView").css("display", "block");
+ 		$("#banner").css("display", "block");
+ 		$("#detailsView").css("display", "block");
+ 		$("#dinnerOverviewView").css("display", "none");
  	});
 }
